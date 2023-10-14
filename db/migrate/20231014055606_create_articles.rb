233ed79360ed8cdbf78ac6cc19f39f1839a6a196
1997722 <asuka.takeuchi.722@gmail.com>
2023-10-14 17:17:27 +0900

    ActiveHashの導入

---
+++ db/migrate/20231014055606_create_articles.rb
@@ -0,0 +1,16 @@
+class CreateArticles < ActiveRecord::Migration[7.0]
+  def change
+    create_table :articles do |t|
+      t.string :product, null: false, default: ""
+      t.integer :price, null: false, default: 0
+      t.integer :category_id, null: false, default: 0
+      t.text :content         
+      t.integer :state_id, null: false, default: 0
+      t.integer :postage_id, null: false, default: 0
+      t.integer :prefecture_id, null: false, default: 0
+      t.integer :shipping_date_id, null: false, default: 0
+      t.references :user, null: false
+      t.timestamps
+    end
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_10_13_102031) do
+ActiveRecord::Schema[7.0].define(version: 2023_10_14_055606) do
   create_table "active_storage_attachments", charset: "utf8", force: :cascade do |t|
     t.string "name", null: false
     t.string "record_type", null: false
@@ -37,6 +37,21 @@ ActiveRecord::Schema[7.0].define(version: 2023_10_13_102031) do
     t.bigint "blob_id", null: false
     t.string "variation_digest", null: false
     t.index ["blob_id", "variation_digest"], name: "index_active_storage_variant_records_uniqueness", unique: true
+  end
+
+  create_table "articles", charset: "utf8", force: :cascade do |t|
+    t.string "product", default: "", null: false
+    t.integer "price", default: 0, null: false
+    t.integer "category_id", default: 0, null: false
+    t.text "content"
+    t.integer "state_id", default: 0, null: false
+    t.integer "postage_id", default: 0, null: false
+    t.integer "prefecture_id", default: 0, null: false
+    t.integer "shipping_date_id", default: 0, null: false
+    t.bigint "user_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_articles_on_user_id"
   end
 
   create_table "items", charset: "utf8", force: :cascade do |t|
