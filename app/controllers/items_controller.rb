6b4b3a09549fd0465ff9033e5c353030edbb8c85
1997722 <asuka.takeuchi.722@gmail.com>
2023-10-14 14:50:50 +0900

    Active Storageの導入

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,4 +1,9 @@
 class ItemsController < ApplicationController
   def index
   end
+
+  private
+
+  def item_params
+    params.require(:item).permit(:product, :proce, :category_id, :content, :state_id, :postage_id, :prefecture_id, :shipping_date_id,:user)
 end
