b8dd3bb5b34a70e4a981c098dfdf771869d1bc9b
1997722 <asuka.takeuchi.722@gmail.com>
2023-10-16 14:54:37 +0900

    Itemモデル単体テスト

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,7 +1,26 @@
 class Item < ApplicationRecord
-  belongs_to :user
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :category
+  belongs_to :postage
+  belongs_to :prefecture
+  belongs_to :shipping_date
+  belongs_to :state
+  belongs_to :user 
   has_one :buy
   has_one_attached :image
 
-  
+  with_options presence: true do
+    validates :content
+    validates :image
+    validates :product
+    validates :price, inclusion: { in: 300..9_999_999 }, format: { with: /\A[0-9]+\z/ }
+  end
+  with_options numericality: { other_than: 1 } do
+    validates :category_id
+    validates :state_id
+    validates :postage_id
+    validates :prefecture_id
+    validates :shipping_date_id
+  end
+  validates :price, numericality: true
 end
