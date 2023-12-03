bf781f2e33ef3b65fe943a87ea03404c8b4be1ba
1997722 <asuka.takeuchi.722@gmail.com>
2023-12-03 21:01:13 +0900

    コントローラー修正

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -11,7 +11,7 @@ class ApplicationController < ActionController::Base
     end
   end
 
-  private
+  
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:family_name,:first_name,:family_name_kana,:first_name_kana,:email,:encrypted_password,:birthday,:nickname ])
   end
